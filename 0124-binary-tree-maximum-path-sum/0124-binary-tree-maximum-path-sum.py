c0cbb5eb981a1d469c1fff29a4d2884f54bb52c2
sharathc21 <97244326+sharathc21@users.noreply.github.com>
2022-11-29 00:15:25 +0530

    Time: 233 ms (13.76%), Space: 21.5 MB (20.21%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -7,21 +7,16 @@
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
         res = [root.val]
-        print("res: ", res, "\n ##########")
         
         def dfs(root):
             if not root:
                 return 0
             
             leftmax = max(dfs(root.left), 0)
-            print("printing LeftMax:  ", leftmax)
             rightmax= max(dfs(root.right), 0)
-            print("printing rightMax:  ", rightmax)
             
             res[0] = max(res[0], root.val + leftmax +rightmax)
-            print("Printing res after calculation: ", res)
-            print("\n $$$$$$$$")
-            print(root.val + max(leftmax, rightmax))
+          
             
             return root.val + max(leftmax, rightmax)
         dfs(root)
